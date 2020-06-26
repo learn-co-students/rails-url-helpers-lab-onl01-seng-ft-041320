b53e60d8093dc56a8c26a2093c203545cf10e863
jaesonwatts <illender.the.great@gmail.com>
2020-06-26 17:30:20 -0600

    Done.

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,5 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean, :default => 1
+    add_column :students, :active, :boolean, :default => 0
   end
 end
